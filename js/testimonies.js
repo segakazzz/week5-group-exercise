9e217e8a3428d6ffc9a85a9922ecf5a3835b340d
Kazue Sasatani <segakazue@gmail.com>
2019-07-30 13:47:51 -0500

    Added more contents

--- js/testimonies.js
+++ js/testimonies.js
@@ -6,7 +6,7 @@ $(function() {
     $(".grid").masonry({
       // options
       itemSelector: ".grid-item",
-      columnWidth: 300,
+      columnWidth: 400,
       fitWidth: true
     });
   };
@@ -98,7 +98,7 @@ $(function() {
     });
     
     $(".testimony.grid-item").on("click", function() {
-      let size = $(this).hasClass("large") ? { width: 600 } : { width: 300 };
+      let size = $(this).hasClass("large") ? { width: 800 } : { width: 400 };
       $(this)
         .toggleClass("large")
         .animate(size, masonry);
